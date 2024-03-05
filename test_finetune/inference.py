77da4ccbd04400b0580deecd6501edbab8a2222e
skvarre <skvaertill@hotmail.com>
2024-03-05 09:50:38 +0000

    Add bos-token depending on start or not

--- test_finetune/inference.py
+++ test_finetune/inference.py
@@ -18,16 +18,17 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 original_model = AutoModelForCausalLM.from_pretrained("AI-Sweden-Models/gpt-sw3-126m")
 
 #TODO: Hardcoded. Assumes bos_token = <s> and eos_token = <|endoftext|>.
-def parse_input(prompt):
-    return f"<|endoftext|>\n<s>User\n{prompt}<s>\n"
+def parse_input(prompt, beginning_of_conversation):
+    parsed_output = f"\n<s>User\n{prompt}\n<s>Bot"
+    return f"<|endoftext|>{parsed_output}" if beginning_of_conversation else parsed_output
+    # return f"<|endoftext|>\n<s>User\n{prompt}\n<s>Bot"
 
-def generate(model, tokenizer, prompt, max_length=100):    
+def generate(model, tokenizer, prompt, max_length=200):    
     inputs = tokenizer.encode(prompt, return_tensors="pt").to(device)
-    outputs = model.generate(inputs, max_length=max_length, do_sample=True, pad_token_id=tokenizer.eos_token_id)
+    outputs = model.generate(inputs, max_length=max_length, do_sample=True, pad_token_id=tokenizer.pad_token_id)
     output = tokenizer.decode(outputs[0], skip_special_tokens=False)
     # Stop at the first <s> token.
     return "".join(output.split('<s>')[0:3])
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -41,14 +42,15 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     print(f"Model loaded on {"GPU" if device == "cuda:0" else "CPU"}.")
 
+    beginning_of_conversation = True # Not sure this is needed. 
     while True:
         print("Type prompt or press ENTER to exit:")
-
         prompt = input()
 
         if prompt == "":
             print("Exiting...")
             exit(0)
         else:
-            parsed_prompt = parse_input(prompt) if args.p == 'True' else prompt
+            parsed_prompt = parse_input(prompt, beginning_of_conversation) if args.p == 'True' else prompt
             print(generate(model, tokenizer, parsed_prompt))
+            beginning_of_conversation = False
