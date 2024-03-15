fe5a9d50d4e030704cfc960c59ddb1a77fabb8c6
skvarre <skvaertill@hotmail.com>
2024-03-15 09:46:42 +0000

    Test spellchecker

---
+++ spellcheck.py
@@ -0,0 +1,35 @@
+"""
+TESTING OF SPELLCHECKER.
+"""
+from spylls.hunspell import Dictionary
+import json
+import re
+
+dictionary = Dictionary.from_files('sv_SE')
+path = "data/2_examples.jsonl"
+
+def score(turns):
+    total = 0
+    score = 0
+    for turn in turns:
+        for _, msg in turn.items():
+            for words in re.split(r'[ ,!?:;"]+' ,msg):
+                total += 1
+                # print(words, dictionary.lookup(words))
+                score += 1 if dictionary.lookup(words) else 0
+    print(score, total, score/total)
+    return score/total
+
+def spellcheck(turns, threshold):
+    return True if score(turns) > threshold else False
+
+#TODO: Implement a new dataset based on the spellchecked data.
+def spellchecked_data(path, threshold):
+    pass
+
+if __name__ == '__main__':
+    with open (path, "r") as file:
+        for line in file:
+            line = json.loads(line)
+            turns = line['text']
+            print(spellcheck(turns, 0.35))
